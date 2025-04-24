7d95ded4d21a2d591acd840716096941bc39d9c1
nideck <arnideck@gmail.com>
2025-04-24 12:35:39 -0300

    update dependencies

--- src/agent/index.js
+++ src/agent/index.js
@@ -1,16 +1,15 @@
 import 'dotenv/config';
 import { executarSQL } from '../tools/executarSql.js';
 import { SchemaInspector } from '../tools/schemaInspector.js';
-import { ChatOpenAI } from 'langchain/chat_models/openai';
-import { OpenAI } from 'langchain/llms/openai';
-import { OpenAIEmbeddings } from 'langchain/embeddings/openai';
+import { ChatOpenAI } from "@langchain/openai";
 import { initializeAgentExecutorWithOptions } from 'langchain/agents';
 
 let executor = null;
 
 export const handler = async (event) => {
   if (!executor) {
-    const model = new ChatOpenAI({ temperature: 0, modelName: 'gpt-4', openAIApiKey: process.env.OPENAI_API_KEY });
+    const model = new ChatOpenAI({ temperature: 0, modelName: 'gpt-4o-mini', openAIApiKey: process.env.OPENAI_API_KEY });
+    
     executor = await initializeAgentExecutorWithOptions([executarSQL, new SchemaInspector()], model, {
       agentType: 'openai-functions',
       verbose: true
