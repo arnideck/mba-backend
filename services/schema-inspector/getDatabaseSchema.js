15e3c969e650a98b8a6339944429139ae64ca3d2
Andre Nideck <arnideck@gmail.com>
2025-04-27 23:04:51 -0300

    fix: updated schema inspector

--- services/schema-inspector/getDatabaseSchema.js
+++ services/schema-inspector/getDatabaseSchema.js
@@ -1,5 +1,12 @@
 import schema from "./dicionario_dados_negocio.json" assert { type: "json" };
 
+let printed = false; // guarda se já imprimiu
+
 export function getSchema() {
+  if (!printed) {
+    console.log("🚀 Conteúdo do JSON carregado:", JSON.stringify(schema, null, 2));
+    printed = true;
+  }
   return schema;
 }
+
